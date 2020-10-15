0c173b05b11649c51fb34cde19427d2f1b4f86f1
Sara Green <72286716+Sara-M-Green@users.noreply.github.com>
2020-10-14 19:31:01 -0700

    completed shopping list challenge

---
+++ script.js
@@ -0,0 +1,25 @@
+// add new item with buttons to shopping list
+$( "#js-shopping-list-form" ).submit(function(event) {
+    event.preventDefault();
+    const newItem = $(this).find('input[name="shopping-list-entry"]').val();
+    $('.shopping-list').append($(`<li><span class="shopping-item">${newItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`));
+    this.reset();
+});
+
+$('ul').on('click', '.shopping-item-delete', function(event){
+    $(this).closest('li').remove(); 
+});
+
+$("ul").on("click", ".shopping-item-toggle", function(event){
+    $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+});
+
+
+
+
+
+            
+          
+        
+
+
